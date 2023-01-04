847673871cbda9d431d16ddc5267879b733c8ebe
Sebastian Baez <sebaez200011@gmail.com>
2023-01-04 12:08:25 -0500

    Make sure creates a membership at the same time a circle does it

--- compartecarro/compartecarro/circles/serializers/circles.py
+++ compartecarro/compartecarro/circles/serializers/circles.py
@@ -7,6 +7,13 @@ from compartecarro.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
 
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000
+    )
+    is_limited = serializers.BooleanField(default=False)
+
     class Meta:
         model = Circle
         fields = (
@@ -16,3 +23,16 @@ class CircleModelSerializer(serializers.ModelSerializer):
             'verified', 'is_public',
             'is_limited', 'members_limit'
         )
+        read_only_fields = (
+            'is_public',
+            'verified',
+            'rides_offered',
+            'rides_taken'
+        )
+
+    def validate(self, data):
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a members limit must be provided.')
+        return data
--- compartecarro/compartecarro/circles/views/circles.py
+++ compartecarro/compartecarro/circles/views/circles.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 # Models
-from compartecarro.circles.models import Circle
+from compartecarro.circles.models import Circle, Membership
 
 # Serializers
 from compartecarro.circles.serializers import CircleModelSerializer
@@ -20,3 +20,16 @@ class CircleModelViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+
+    def perform_create(self, serializer):
+
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
